1c9da83575e2e6800e28a3445a1ae09701e9fabc
Arnav Saraswat <arnavsaraswat@Mac.lan>
2025-01-01 17:59:45 +0530

    done

--- 1toN.cpp
+++ 1toN.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
+void solve(int n){
+    if (n>0){
+        solve(n-1);
+        cout<<n<<" ";
+    }
+    return;
+}
 int main(){
-    cout<<"hello world";
+    int n;
+    cin>>n;
+    solve(n);
     return 0;
 }
